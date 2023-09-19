7f3e2ce20a2740c6eab976690d8f46053f470609
Nikhil lama <nikhil.lama@innovatetech.co>
2023-09-19 22:29:37 +0545

    init: intial commit

---
+++ marketPlace/marketplace.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, render_template
+import grpc
+
+from marketPlace.generated.recommendation_pb2 import BookCategory, RecommendationRequest
+from marketPlace.generated.recommendation_pb2_grpc import RecommendationsStub
+
+
+app = Flask(__name__)
+
+# just for demo keeping on global
+recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
+recommendations_channel = grpc.insecure_channel(f"{recommendations_host}:50051")
+recommendations_client = RecommendationsStub(recommendations_channel)
+
+
+@app.route("/")
+def render_homepage():
+    recommendations_request = RecommendationRequest(
+        user_id=1, category=BookCategory.SELF_HELP, max_results=3
+    )
+    # calling req from client
+    recommendations_response = recommendations_client.Recommend(recommendations_request)
+    return render_template(
+        "homepage.html", recommendations=recommendations_response.recommendations
+    )
---
+++ recommendations/recommendations.py
@@ -0,0 +1,58 @@
+from concurrent import futures
+import random
+
+import grpc
+
+from recommendations.generated.recommendation_pb2 import (
+    BookCategory,
+    BookRecommendation,
+    RecommendationResponse,
+)
+import recommendations.generated.recommendation_pb2_grpc as recommendation_pb2_grpc
+
+
+books_by_category = {
+    BookCategory.MYSTERY: [
+        BookRecommendation(id=1, title="Hello"),
+        BookRecommendation(id=2, title="Hello2"),
+        BookRecommendation(id=3, title="Hello3"),
+    ],
+    BookCategory.SCIENCE_FICTION: [
+        BookRecommendation(id=4, title="Hello4"),
+        BookRecommendation(id=5, title="Hello5"),
+        BookRecommendation(id=6, title="Hello6"),
+    ],
+    BookCategory.SELF_HELP: [
+        BookRecommendation(id=7, title="Hello7"),
+        BookRecommendation(id=8, title="Hello8"),
+        BookRecommendation(id=9, title="Hello9"),
+    ],
+}
+
+
+class RecommendationService(recommendation_pb2_grpc.RecommendationsServicer):
+    def Recommend(self, request, context):
+        if request.category not in books_by_category:
+            context.abort(grpc.StatusCode.NOT_FOUND, "Category not found")
+
+        books_for_category = books_by_category[request.category]
+        num_results = min(request.max_results, len(books_for_category))
+        books_to_recommend = random.sample(books_for_category, num_results)
+
+        return RecommendationResponse(recommendations=books_to_recommend)
+
+
+def serve():
+    # use 10 threads to serve requests
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    recommendation_pb2_grpc.add_RecommendationsServicer_to_server(
+        RecommendationService(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    print("i have started")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
