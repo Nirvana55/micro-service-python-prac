62784c8a3ece72cbc394a48f7521c67ab1aae611
Nikhil lama <nikhil.lama@innovatetech.co>
2023-09-26 23:40:26 +0545

    refactor: update main and recommendation module for prisma

--- recommendations/services/recommendation.py
+++ recommendations/services/recommendation.py
@@ -1,65 +1,36 @@
-from concurrent import futures
-import random
 import sys
-from dotenv import load_dotenv
-import grpc
 import os
-
-load_dotenv()
 
 sys.path.append(os.getenv("PYTHON_PATH_RECOMMENDATIONS"))
 
+from dotenv import load_dotenv
+import grpc
 from generated.recommendation_pb2 import (
-    BookCategory,
-    BookRecommendation,
     RecommendationResponse,
+    GetAllBookRecommendationResponse,
+    CreateBookResponse,
 )
 from generated.recommendation_pb2_grpc import (
     RecommendationsServicer,
-    add_RecommendationsServicer_to_server,
 )
+from controllers import books
 
-
-books_by_category = {
-    BookCategory.MYSTERY: [
-        BookRecommendation(id=1, title="Hello"),
-        BookRecommendation(id=2, title="Hello2"),
-        BookRecommendation(id=3, title="Hello3"),
-    ],
-    BookCategory.SCIENCE_FICTION: [
-        BookRecommendation(id=4, title="Hello4"),
-        BookRecommendation(id=5, title="Hello5"),
-        BookRecommendation(id=6, title="Hello6"),
-    ],
-    BookCategory.SELF_HELP: [
-        BookRecommendation(id=7, title="Hello7"),
-        BookRecommendation(id=8, title="Hello8"),
-        BookRecommendation(id=9, title="Hello9"),
-    ],
-}
+load_dotenv()
 
 
 class RecommendationService(RecommendationsServicer):
     def Recommend(self, request, context):
-        if request.category not in books_by_category:
-            context.abort(grpc.StatusCode.NOT_FOUND, "Category not found")
+        book = books.GetBook(request.book_id)
+        if len(book) == 0:
+            context.abort(grpc.StatusCode.NOT_FOUND, "Book not found")
 
-        books_for_category = books_by_category[request.category]
-        num_results = min(request.max_results, len(books_for_category))
-        books_to_recommend = random.sample(books_for_category, num_results)
+        return RecommendationResponse(recommendation=book)
 
-        return RecommendationResponse(recommendations=books_to_recommend)
+    def GetAllBooksRecommend(self, request, context):
+        books = books.GetBooks()
+        return GetAllBookRecommendationResponse(recommendation=books)
 
-
-def serve():
-    # use 10 threads to serve requests
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_RecommendationsServicer_to_server(RecommendationService(), server)
-    server.add_insecure_port("[::]:50051")
-    server.start()
-    print("i have started")
-    server.wait_for_termination()
-
-
-if __name__ == "__main__":
-    serve()
+    async def CreateBook(self, request, context):
+        createBook = await books.CreateBook(request)
+        print(createBook)
+        return CreateBookResponse(recommendation=createBook)
