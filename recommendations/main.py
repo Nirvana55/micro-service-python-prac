9c065567cec95c9a70e09d7c7bca85452550c752
Nikhil lama <nikhil.lama@innovatetech.co>
2023-10-01 13:56:56 +0545

    refactor: update linting issues and directory name

--- recommendations/main.py
+++ recommendations/main.py
@@ -1,26 +1,34 @@
+"""Module main."""
 import asyncio
+from concurrent import futures
 from prisma import Prisma
 import grpc
-from concurrent import futures
-from generated.recommendation_pb2_grpc import add_RecommendationsServicer_to_server
+from generated_recommendations.recommendation_pb2_grpc import (
+    add_RecommendationsServicer_to_server,
+)
 from services.recommendation import RecommendationService
 
 
 async def main() -> None:
+    """main db"""
     try:
-        db = Prisma(auto_register=True)
-        await db.connect()
+        database = Prisma(auto_register=True)
+        await database.connect()
         server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
         add_RecommendationsServicer_to_server(RecommendationService(), server)
         server.add_insecure_port("[::]:50051")
         await server.start()
         print("i have started")
         await server.wait_for_termination()
-    except Exception as e:
-        print(f"An error occurred {e}")
+    except ImportError as error:
+        print(f"An import error occurred {error}")
+    except ConnectionError as error:
+        print(f"An connection error occurred {error}")
+    except grpc.RpcError as error:
+        print(f"An rpc error occurred {error}")
     finally:
-        await db.disconnect()
-        await server.stop()
+        await database.disconnect()
+        await server.stop(None).wait()
         print("i am disconnected")
 
 
