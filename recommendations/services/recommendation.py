847b8ce96865cd9d8ccf8b0ef80773d7c89949ef
Nikhil lama <nikhil.lama@innovatetech.co>
2023-10-01 22:20:21 +0545

    refactor: add pydantic for validation error

--- recommendations/services/recommendation.py
+++ recommendations/services/recommendation.py
@@ -9,6 +9,7 @@ from generated_recommendations.recommendation_pb2_grpc import (
 )
 from controllers import books
 from helpers.exception_helper import catch_exception
+from validators.recommendation_validator import title_unique, unique_id
 
 
 sys.path.append(os.getenv("PYTHON_PATH_RECOMMENDATIONS"))
@@ -20,9 +21,7 @@ class RecommendationService(RecommendationsServicer):
 
     @catch_exception
     async def RecommendBook(self, request, context):
-        book = await books.get_book(request.book_id)
-        if book is None:
-            return await context.abort(grpc.StatusCode.NOT_FOUND, "Book not found")
+        book = await unique_id(request.book_id)
         return pb2.RecommendationResponse(**dict(book))
 
     @catch_exception
@@ -44,15 +43,18 @@ class RecommendationService(RecommendationsServicer):
 
     @catch_exception
     async def CreateBook(self, request, context):
+        await title_unique(request.title)
         created_book = await books.create_book(request)
         return pb2.CreateBookResponse(**dict(created_book))
 
     @catch_exception
     async def UpdateBook(self, request, context):
+        await unique_id(request.id)
         updated_books = await books.update_book(request.id, request)
         return pb2.UpdateBookResponse(**dict(updated_books))
 
     @catch_exception
     async def DeleteBook(self, request, context):
+        await unique_id(request.id)
         deleted_book = await books.delete_book(request.id)
         return pb2.DeleteBookResponse({"message": deleted_book})
